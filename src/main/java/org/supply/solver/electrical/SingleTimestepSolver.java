8259679ab82523b85cc5fb6a633ed6caa9cfc635
Morito <marioflagos@gmail.com>
2026-08-30 14:36:58 +0200

    Include study ID in result file names

--- src/main/java/org/supply/solver/electrical/SingleTimestepSolver.java
+++ src/main/java/org/supply/solver/electrical/SingleTimestepSolver.java
@@ -218,14 +218,14 @@ public final class SingleTimestepSolver {
                         linearSystemSolver.solveVoltages(system);
             } catch (IllegalArgumentException e) {
 
-//                if (!e.getMessage().startsWith("Singular admittance matrix")) {
-//                    throw e;
-//                }
+                if (!e.getMessage().startsWith("Singular admittance matrix")) {
+                    throw e;
+                }
 
-                System.out.println(
-                        "SINGULAR at iteration=" + iteration
-                                + ", voltageGuessByTrain=" + voltageGuessByTrain
-                );
+//                System.out.println(
+//                        "SINGULAR at iteration=" + iteration
+//                                + ", voltageGuessByTrain=" + voltageGuessByTrain
+//                );
 
                 return new NetworkResult(
                         previousVoltages,
